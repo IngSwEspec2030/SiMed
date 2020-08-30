4671666bda27dbedf8fbda4bbb484624062a4c88
Johann <joxpg@hotmail.com>
2020-08-30 18:54:57 -0500

    Se agregan fuentes de fronend

---
+++ application/frontend/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { PagesModule } from './pages/pages.module';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NoPageFoundComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
